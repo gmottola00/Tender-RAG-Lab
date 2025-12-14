63caef678d84546fe5fab44623c175311b84b0b2
gmottola00 <gianmarcomottola00@gmail.com>
2025-12-14 10:47:04 +0100

    update db session and client

--- db/session.py
+++ db/session.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from app.core.config import settings
+from configs.config import settings
 from typing import AsyncGenerator
 
 engine = create_async_engine(
